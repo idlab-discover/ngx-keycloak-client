3027f7541ade0d9b894e8008fcae6d9e485f1a34
Thomas Dupont <thomas.dupont@ugent.be>
2022-02-25 12:32:42 +0100

    refactor: add initial service

--- projects/ngx-keycloak-client/src/lib/ngx-keycloak-client.component.ts
+++ projects/ngx-keycloak-client/src/lib/ngx-keycloak-client.component.ts
@@ -2,8 +2,8 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'lib-ngx-keycloak-client',
-  templateUrl: './ngx-keycloak-client.component.html',
-  styleUrls: ['./ngx-keycloak-client.component.scss']
+  template: '',
+  styles: []
 })
 export class NgxKeycloakClientComponent implements OnInit {
 
--- projects/ngx-keycloak-client/src/public-api.ts
+++ projects/ngx-keycloak-client/src/public-api.ts
@@ -2,6 +2,12 @@
  * Public API Surface of ngx-keycloak-client
  */
 
+import { fromEventPattern } from 'rxjs';
+
 export * from './lib/ngx-keycloak-client.service';
 export * from './lib/ngx-keycloak-client.component';
 export * from './lib/ngx-keycloak-client.module';
+
+export * from './lib/auth.service';
+
+export * from './lib/utils';
